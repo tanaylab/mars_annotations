16b09d817c3c5622f986ecdfefc6cf9d0a55f39e
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-04-29 16:46:38 +0300

    added orycun2 gene annotations and orycun3

--- oryCun2/build_intervals.R
+++ oryCun2/build_intervals.R
@@ -1,29 +1,70 @@
 #!/usr/bin/env Rscript
 library(dplyr, warn.conflicts = FALSE)
 
-.misha_root <- "/net/mraid14/export/tgdata/db/tgdb/orycun2/trackdb"
+.misha_root <- "/net/mraid14/export/tgdata/db/tgdb/orycun2_contigs/"
 
 
 ########################################################################
 main <- function(argv) {
-    if (length(argv) != 2) {
-        cat("Usage: gene_intervals.R  <refseq.txt.gz>  <output.tsv>\n", file = stderr())
+    if (length(argv) != 3) {
+        cat("Usage: gene_intervals.R  <refseq.txt.gz>  <xeno.txt>  <output.tsv> \n", file = stderr())
         return(2)
     }
 
+    message("Loading misha database")
     misha::gdb.init(.misha_root)
 
+    message("Reading files")
     gene_tab <- read_refseq(argv[1])
-    gene_tab <- merge_genes(gene_tab)
+    xeno <- read_refseq(argv[2], alt_name=TRUE)
 
-    data.table::fwrite(gene_tab, argv[2], sep = "\t")
+    message("Merging other genes and Refseq")
+    xeno <- xeno %>% 
+        filter(chrom %in% gintervals.all()$chrom) %>% 
+        filter(gene_name != "") %>%         
+        mutate(gene_name = toupper(gene_name))
+    gene_tab <- gene_tab %>% filter(chrom %in% gintervals.all()$chrom)
+
+    # Remove from 'other' genes that are present in the refseq
+    xeno <- xeno %>% filter(!(gene_name %in% gene_tab$gene_name))
+
+    # Exclude from 'other' genes that are within 1k from a refseq gene
+    nn <- xeno %>% misha.ext::gintervals.neighbors1(gene_tab)
+    xeno_tab <- nn %>% filter(abs(dist) > 1e3 | is.na(dist)) %>% distinct(chrom, start, end, strand, gene_name, alt_name)
+    xeno_tab <- annotate_organism(xeno_tab)
+
+    # We first take the refseq gene intervals, and then we take non overlapping genes from OtherRefSeq
+    gene_tab_all <- bind_rows(gene_tab %>% mutate(type = "refseq") %>% mutate(organism = "orycun2"), xeno_tab %>% mutate(type = "other")) %>% arrange(chrom, start, end, strand)    
+    data.table::fwrite(gene_tab_all %>% distinct(gene_name, type, organism), "gene_types.tsv", sep = "\t", quote=FALSE)    
+
+
+    message("Merging genes")
+    gene_tab_all <- merge_genes(gene_tab_all) %>% filter(!is.na(gene_name))
+
+    message("Writing")
+    data.table::fwrite(gene_tab_all, argv[3], sep = "\t", quote=FALSE)    
 
     return(0)
 }
 
 
+annotate_organism <- function(df){
+    hg_gene <- fread("/net/mraid14/export/tgdata/db/tgdb/hg19/annots/refGene.txt", select=2, col.names="alt_name") %>% mutate(organism = "hg19") %>% distinct(alt_name, organism)
+    mouse_gene <- fread("/net/mraid14/export/tgdata/db/tgdb/mm9/annots/refGene.txt", select=2, col.names="alt_name") %>% mutate(organism = "mm9") %>% distinct(alt_name, organism)
+
+    df <- xeno_tab %>% 
+        left_join(mouse_gene, by = "alt_name") %>% 
+        left_join(hg_gene %>% 
+            rename(organism1 = organism), by = "alt_name" ) %>% 
+        mutate(organism = ifelse(is.na(organism) & !is.na(organism1), organism1, organism))  %>% 
+        select(chrom:organism)
+
+    return(df)
+}
+
+
 ########################################################################
-read_refseq <- function(fname) {
+read_refseq <- function(fname, alt_name=FALSE) {
     cnames <- c(
         "bin",
         "name",
@@ -45,15 +86,22 @@ read_refseq <- function(fname) {
 
     refseq <- data.table::fread(fname, header = FALSE, col.names = cnames, data.table = FALSE)
 
-    refseq <- refseq %>% filter(grepl("^chr[0-9X]+$", chrom))
+    # refseq <- refseq %>% filter(grepl("^chr[0-9X]+$", chrom))
     refseq <- refseq %>% mutate(strand = case_when(strand == "+" ~ +1L, strand == "-" ~ -1L))
     refseq <- refseq %>%
               mutate(exonStarts = stringr::str_remove(exonStarts, ",$"),
                      exonEnds = stringr::str_remove(exonEnds, ",$")) %>%
               tidyr::separate_rows(exonStarts, exonEnds, sep = ",") %>%
               mutate(exonStarts = as.integer(exonStarts), exonEnds = as.integer(exonEnds))
-    refseq <- refseq %>%
+
+    if (alt_name){
+        refseq <- refseq %>%
+              select(chrom, start = exonStarts, end = exonEnds, strand, gene_name = name2, alt_name = name)
+    } else {
+        refseq <- refseq %>%
               select(chrom, start = exonStarts, end = exonEnds, strand, gene_name = name2)
+    }
+    
 
     return(refseq)
 }
@@ -67,6 +115,7 @@ merge_genes <- function(gene_tab) {
     )
     pairs <- pairs %>%
              distinct()
+    
     pairs <- igraph::graph_from_edgelist(as.matrix(pairs), directed = FALSE)
     pairs <- igraph::components(pairs)
     pairs <- tibble::tibble(gene_name = names(pairs$membership), component = pairs$membership)
@@ -110,12 +159,13 @@ merge_genes <- function(gene_tab) {
 
 
 ########################################################################
-gene_pairs <- function(gene_tab) {
+gene_pairs <- function(gene_tab) {    
     canonic_intervals <- misha::gintervals.canonic(gene_tab, unify_touching_intervals = TRUE)
     gene_tab <- gene_tab %>% mutate(canonic = attr(canonic_intervals, "mapping"))
 
     pairs <- gene_tab %>%
              distinct(gene_name, canonic)
+    # Find places where unified intervals contain more than one gene
     pairs <- pairs %>%
              rename(gene1 = gene_name) %>%
              left_join(pairs %>% rename(gene2 = gene_name), by = "canonic") %>%
---
+++ oryCun2/curate_genes.R
@@ -0,0 +1,128 @@
+library(tgutil)
+library(tidyverse)
+
+umis_dir <- "/net/mraid14/export/tgdata/db/tgdb/mars_runs/stelzer_gene_annot/plates/umis/"
+annots_file <- "/net/mraid14/export/tgdata/db/tgdb/mars_runs/stelzer_gene_annot/work/210426_03/scdb_oryCun2/annotations/gene_intervals_oryCun2.txt"
+
+
+# library(metacell)
+# dataset_table_fn <- "/net/mraid14/export/tgdata/users/atanay/proj/rabemb/config/samp_key_190421.txt"
+# scdb_init("scrna_db", force_reinit=T)
+# mcell_import_multi_mars(mat_nm = "embs",
+#                      dataset_table_fn = dataset_table_fn,
+#                      base_dir = umis_dir,
+#                      patch_cell_name = TRUE,
+#                      force=TRUE)
+
+
+umi_files <- list.files(umis_dir, full.names=TRUE)
+
+doMC::registerDoMC(20)
+marginals <- plyr::llply(umi_files, function(x) tgutil::fread_rownames(x) %>% as.data.frame() %>% filter(!is.na(rowname)) %>% column_to_rownames("rowname") %>% as.matrix() %>% rowSums(), .parallel=TRUE) 
+
+mars <- do.call('cbind', marginals) %>% rowSums() %>% enframe("gene_name", "tot_umis") %fcache_df% "tot_umis_gene.tsv" %>% as_tibble()
+
+gene_types <- fread("gene_types.tsv") %>% 
+    # keep a single organism per gene
+    mutate(organism = factor(organism, levels=c("orycun2", "mm9", "hg19"))) %>% 
+    arrange(gene_name, organism) %>% 
+    distinct(gene_name, .keep_all=TRUE) %>% 
+    as_tibble()
+
+# The policy is: 
+# 1. If there is orycun refseq - use it
+# 2. If there is "other" gene from mouse or human annotation - take it (all the genes separated by ";", first mouse and then human)
+# 3. If there is a single "other" name (even from other organisms) - take it. 
+
+mars_types <- mars %>% 
+    distinct(gene_name)  %>%     
+    # save the orginial long name in "gene_name_long" column
+    mutate(gene_name_long = gene_name) %>%
+    # suggest an alternative name by removing RIKEN,LOC,ORF etc. genes    
+    mutate(gene_name = gsub(".*\\d+.*\\d+RIK;*","",gsub("LOC\\d+;*","",gene_name))) %>% 
+    mutate(gene_name = gsub("C\\d+ORF\\d+;*","",gsub("^\\d+.*;","",gene_name))) %>%  
+    separate_rows("gene_name", sep=";") %>% 
+    mutate(gene_name = ifelse(gene_name == "", NA, gene_name)) %>% 
+    # add organism annotation
+    left_join(gene_types, by = "gene_name")
+
+# Add a genome suffix for the 'other' genes
+mars_types <- mars_types %>%     
+    mutate(gene_name = case_when(
+        organism == "mm9" & !is.na(gene_name) ~ paste0(gene_name, "_mm9"),
+        organism == "hg19" & !is.na(gene_name) ~ paste0(gene_name, "_hg19"),
+        is.na(organism) & !is.na(gene_name) ~ paste0(gene_name, "_other"),
+        TRUE ~ gene_name # for orycun genes do not add a suffix
+    )) 
+
+mars_types <- mars_types %>%             
+    arrange(gene_name_long, organism) %>% 
+    group_by(gene_name_long) %>% 
+    # for each long name - extract the number of refseq and "other" genes
+    summarise(        
+        refseq = paste(sort(unique(gene_name[type == "refseq" & !is.na(type)])), collapse=";"), 
+        other =  paste(sort(unique(gene_name[type == "other" & !is.na(type)])), collapse=";"),
+
+        # if we have an organism (note that names are sorted by the organism factor) - take all the genes from it (separated by '"'', can be either orycun refseq or other mm9 and hg19, but only from a single organism)        
+        new_name = ifelse(!is.na(organism[1]), paste(gene_name[organism == organism[1] & !is.na(organism)], collapse=";"), NA),
+
+        # if there are no "other" genes - use the concatenation of the refseq genes
+        new_name = ifelse(is.na(new_name) & other == "", gene_name_long, new_name)
+    ) %>%     
+    mutate(new_name = ifelse(new_name == '', NA, new_name))
+    
+# if we have a single "other" name - use it. 
+mars_types <- mars_types %>%         
+    mutate(new_name = ifelse(is.na(new_name) & !grepl(";", other) & !is.na(other), other, new_name)) 
+
+# save the data frame
+mars_types <- mars_types %>%     
+    rename(gene_name=gene_name_long) %>%     
+    right_join(mars) %fcache_df% "tot_umis_gene_types.tsv"
+
+trans_tab <- mars_types %>% 
+    mutate(more_than_one_gene = grepl(";", new_name), other_organism = grepl("_mm9", new_name) | grepl("_hg19", new_name)) %>% 
+    filter(tot_umis > 10 & (is.na(new_name) | (more_than_one_gene & other_organism))) %>% 
+    select(-more_than_one_gene, -other_organism) %>% 
+    arrange(desc(tot_umis)) %fcache_df% "missing_gene_annots.tsv"
+
+# At this point we went manually and curated the translation table. We now merge the output with a suffix of "_m"
+trans_table_manual <- readxl::read_xlsx("missing_gene_annots.xlsx")
+
+# Use the following heuristic - take the names with the least number characters excluding names with parenthesis
+trans_table_curated <- trans_table_manual %>% 
+    separate_rows(new_name, sep=";")  %>% 
+    mutate(bad = grepl("\\(", new_name)) %>% 
+    mutate(numbers = map(str_extract_all(gsub("_hg19", "", new_name) %>% gsub("_mm9", "", .), "(\\d+)"), str_length) %>% map_dbl(max)) %>% 
+    arrange(gene_name, as.numeric(bad), numbers)  %>% 
+    group_by(gene_name) %>% 
+    summarise(ADD = new_name[1])
+
+writexl::write_xlsx(trans_table_curated, "missing_gene_annots_curated.xlsx")
+
+mars_types <- mars_types %>% 
+    left_join(trans_table_curated %>% select(gene_name, ADD) %>% mutate(ADD = paste0(ADD, "_m"))) %>% 
+    mutate(new_name = ifelse(!is.na(ADD), ADD, new_name)) %>% 
+    select(-ADD)
+
+# remove not-curated genes that are not sufficiently expressed
+mars_types <- mars_types %>% filter(!(tot_umis <= 10 & is.na(new_name))) %fcache_df% "tot_umis_gene_types_curated.tsv"
+
+gene_annots <- fread(annots_file) %>% as_tibble()
+
+# we remove genes that were not expressed (less than 11 umis) and didn't come from refseq
+new_gene_annots <- gene_annots %>% 
+    left_join(mars_types %>% select(gene_name, new_name, tot_umis), by = "gene_name") %>% 
+    mutate(other_organism = grepl("_mm9", new_name) | grepl("_hg19", new_name) | grepl("_other", new_name)) %>% 
+    filter(tot_umis > 10 | !other_organism) %>% 
+    select(chrom, start, end, strand, gene_name = new_name) %>% 
+    arrange(chrom, start, end, strand) %>% 
+    mutate(start = as.numeric(start), end = as.numeric(end), strand = as.numeric(strand)) %>% 
+    filter(!is.na(start), !is.na(gene_name), gene_name != "gene_name")    
+
+fwrite(new_gene_annots, "curated_gene_annots.tsv", sep="\t")
+
+ercc_intervs <- fread('ERCC_intervals.tsv.gz') %>% as_tibble()
+
+fwrite(bind_rows(new_gene_annots, ercc_intervs), "gene_intervals_oryCun2_with_ERCC_curated.txt", sep="\t", quote=FALSE)
+    
